9040769a18ca20f5173e07b473a01fa666c10b36
CTemp3 <92496739+CTemp3@users.noreply.github.com>
2021-11-28 21:52:42 -0700

    can't get the save functionality working, I feel like there's something obvious I'm missing

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,25 +1,54 @@
+var schedule = {};
 
 
 
+// initialise session upon loading
+var sessionInit = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"))
 
-// save button was clicked
-$(".btn-primary").click(function() {
-    // get form values
-    var taskText = $("#modalTaskDescription").val();
-    var taskDate = $("#modalDueDate").val();
-  
-    if (taskText && taskDate) {
-      createTask(taskText, taskDate, "toDo");
-  
-      // close modal
-      $("#task-form-modal").modal("hide");
-  
-      // save in tasks array
-      tasks.toDo.push({
-        text: taskText,
-        date: taskDate
-      });
-  
-      saveTasks();
-    }
+
+};
+
+// function for saving tasks
+var saveSchedule = function() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+  };
+
+// edit a paragraph element
+$(".list-group").on("click", "p", function() {
+    var text = $(this)
+      .text()
+      .trim();
+    var textInput = $("<textarea>")
+      .addClass("form-control")
+      .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
 });
+
+  // save edits
+$(".list-group").on("blur", "textarea", function() {
+    // get the text area's current value/text
+    var text = $(this)
+      .val()
+      .trim();
+
+    // get parent ul's id
+    var status = $(this)
+        .closest(".list-group-item")
+        .index();
+  
+    // save to local storage
+    schedule[status].text = text;
+    saveSchedule();
+  
+    // recreate p element
+    var taskP = $("<p>")
+      .addClass("col-10 past-time list-group-item")
+      .text(text);
+
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
+});
+
+sessionInit();
